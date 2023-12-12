fc7fbc06c19e0a797adbe19a835db0d222cb2106
PAO <natthachai_bubpa@cmu.ac.th>
2023-12-12 16:09:08 +0700

    โค้ดเดิมรัน10รอบไม่ผ่านผ่านรอบที่11

--- lab8_3.cpp
+++ lab8_3.cpp
@@ -1,3 +1,62 @@
+#include<iostream>
+#include<iomanip>
+#include<string>
+using namespace std;
+
+
+int main(){
+    int A,H,B;
+    cout << "Enter your age: ";
+    cin >> A ;
+    
+
+
+
+    if (A <= 25){
+        cout << "Enter your height: ";
+        cin >> H ;
+        
+        if(H<100){
+                    cout << "Your character = Chopper";
+                }
+                else{
+                    if(H<180){
+                        cout << "Your character = Usopp";
+                    }
+                    else{
+                        cout << "Enter your bounty: ";
+                        cin >> B ;
+                        
+                        if(B>1100000000){
+                            cout << "Your character = Zoro";
+                        }
+                        else{
+                            cout << "Your character = Sanji";
+                        }
+                    }
+                }  
+    }
+
+
+
+    else {
+        if (A <= 60){
+            cout << "Enter your bounty: ";
+            cin >> B ;
+            
+                if(B>500000000){
+                    cout << "Your character = Jinbe";
+                }
+                else{
+                    cout << "Your character = Franky";
+                }
+        }
+        else{
+            cout << "Your character = Brook";
+        }
+    }
+}
+
 /*
 "Enter your age: "
 "Enter your height: "
